46185d235d4a820c4ccf83bfb169e23719dc9f9e
segalzeyalz <segaleseo@gmail.com>
2019-02-02 23:53:26 +0200

    design the table

--- src/containers/Portfolio/index.js
+++ src/containers/Portfolio/index.js
@@ -6,30 +6,41 @@ import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
+import SvgIcon from '@material-ui/core/SvgIcon';
 import CSS from './Portfolio.css';
 
 class Portfolio extends Component {
     render(){
+
         let { stocks } = this.props;
         return (<div className={CSS.Portfolio}>
                 <h2>My Portfolio</h2>
                    <Table>
                        <TableHead>
-                           <TableCell>symbol</TableCell>
-                           <TableCell>name</TableCell>
+                           <TableCell>Symbol</TableCell>
+                           <TableCell>Name</TableCell>
+                           <TableCell>Purchased quantity</TableCell>
+                           <TableCell>Purchase price</TableCell>
                            <TableCell>startOfCommerce</TableCell>
                            <TableCell>currentPrice</TableCell>
+                           <TableCell>Profit</TableCell>
                            <TableCell>sell</TableCell>
-                           <TableCell>sell</TableCell>
-                           hand-holding-usd
                        </TableHead>
                      <TableBody>
                        {stocks.map(elem=> {
                            return <TableRow>
                                     <TableCell>{elem.symbol}</TableCell>
                                     <TableCell>{elem.name}</TableCell>
+                                    <TableCell>{elem.qty}</TableCell>
+                                    <TableCell>{elem.price}</TableCell>
                                     <TableCell>{elem.startOfCommerce}</TableCell>
                                     <TableCell>{elem.currentPrice}</TableCell>
+                                    <TableCell>{elem.profit}</TableCell>
+                                    <TableCell>
+                                        <SvgIcon>
+                                            <path d="M9.56 8.1c-1.6-.51-2.66-.71-2.66-1.88 0-.83.72-1.62 2.1-1.62 1.59 0 2.1.88 2.1 1.94H13c0-1.79-1.17-3.09-3-3.44V1H8v2.11c-1.58.32-3 1.37-3 3.12 0 2.25 1.78 2.8 4 3.52 1.88.61 2.25 1.04 2.25 2.09 0 .9-.67 1.56-2.25 1.56-1.2 0-2.25-.84-2.25-2.06h-2c0 1.88 1.38 3.2 3.25 3.56V17h2v-2.07c2.04-.29 3.2-1.49 3.2-3.1 0-1.87-.94-2.87-3.64-3.73z"/>
+                                        </SvgIcon>
+                                    </TableCell>
                                  </TableRow>
                         }
                     )}</TableBody>
