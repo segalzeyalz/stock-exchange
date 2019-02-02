1e7859f36ff72f05d8c146b221424e55a56292f0
segalzeyalz <segaleseo@gmail.com>
2019-02-02 23:21:08 +0200

    make the table

--- src/containers/Portfolio/index.js
+++ src/containers/Portfolio/index.js
@@ -1,18 +1,50 @@
 import React, { Component } from 'react';
-import * as actionTypes from './../../constants/stockActions';
+import * as actionTypes from './../../constants/UIActions';
 import { connect } from 'react-redux';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
 import CSS from './Portfolio.css';
 
 class Portfolio extends Component {
     render(){
+        let { stocks } = this.props;
         return (<div className={CSS.Portfolio}>
-                    <h1>Portfolio</h1>
+                <h2>My Portfolio</h2>
+                   <Table>
+                       <TableHead>
+                           <TableCell>symbol</TableCell>
+                           <TableCell>name</TableCell>
+                           <TableCell>startOfCommerce</TableCell>
+                           <TableCell>currentPrice</TableCell>
+                           <TableCell>sell</TableCell>
+                       </TableHead>
+                     <TableBody>
+                       {stocks.map(elem=> {
+                           return <TableRow>
+                               <TableCell>{elem.symbol}</TableCell>
+                           <TableCell>{elem.name}</TableCell>
+                           <TableCell>{elem.startOfCommerce}</TableCell>
+                           <TableCell>{elem.currentPrice}</TableCell>
+                           </TableRow>
+                        }
+                    )}</TableBody>
+                   </Table>
                 </div>)
     }
 }
   
-  const mapDispatchToProps = dispatch => {
+const mapStateToProps = state => {
     return {
+        stocks:state.stocks.stocks
     }
 }
-export default connect(null, mapDispatchToProps)(Portfolio);
+
+const mapDispatchToProps = dispatch => {
+    return {
+      onOpen: () => dispatch({type:actionTypes.OPEN_POPUP}),
+    }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Portfolio);
--- src/containers/SearchBar/index.js
+++ src/containers/SearchBar/index.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
 import * as actionTypes from './../../constants/stockActions';
 import { connect } from 'react-redux';
+import Autosuggest from 'react-autosuggest';
+
 import CSS from './SearchBar.css';
 
 class SearchBar extends Component {
     render(){
         return (<div className={CSS.SearchContainer}>
                     <input onKeyDown={(e)=>this.props.onKeyDown(e)} type="text" placeholder="Search.." className={CSS.SearchInput}/>
+
                 </div>)
     }
 }
-  
+   const mapStateToProps = state => {
+        return {
+            stocks:state.stocks.stocks,
+            searchStock:state.stocks.searchStock
+        };
+    };
   const mapDispatchToProps = dispatch => {
     return {
       onKeyDown: (e) => dispatch({type:actionTypes.FILTER_AVAILABLE_STOCKS, val:e.target.value}),
     }
 }
-export default connect(null, mapDispatchToProps)(SearchBar);
+export default connect(mapStateToProps, mapDispatchToProps)(SearchBar);
